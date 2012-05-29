24964c430563dd2f0fba186a50f6e6cf0290d3ff
Mayank Jani <mayank4net@gmail.com>
2012-05-29 17:21:39 +0530

    second commit

---
+++ city.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_POST) && $_POST['save'])
+{
+	$city = $_POST['city'];
+
+	$short = $_POST['shortcode'];
+
+	$con = mysql_connect('localhost','root','root') or die("error");
+	mysql_select_db("citydatabase", $con) or die("error");
+	$sql = "insert into city_table(name,short_name) values('$city','$short')";
+
+	mysql_query($sql);
+}
+?>
+<form name="city" action="city.php" method="post">
+City: <input type="text" name="city"><br />
+Short Code: <input type="text" name="shortcode"><br />
+<input type="submit" name="save" value="SAVE">
+</form>
+
